e831c855e70e8d135f27f7c2180f7007ca9e073b
santiagoHV <santiherreravelas@gmail.com>
2020-08-15 19:24:08 -0500

    commitIns

--- src/controlador/servlet/ServletIngreso.java
+++ src/controlador/servlet/ServletIngreso.java
@@ -18,18 +18,20 @@ public class ServletIngreso extends HttpServlet {
         super.doGet(req, resp);
         DBAlumno idaDB = new DBAlumno();
         DBProfesor idpDB = new DBProfesor();
+        //estudiantes 20201020---
+        //profesores 1020---
         //----------VALIDACION----------------------------------------------------------
         try {
             ResultSet res = idaDB.getAlumnoById("*ID INGRESADO*");
             if (res.getString("contraseña_a") != "*CONTRASEÑA INGRESADA*") {
-                resp.sendRedirect("index.jsp");
-            }
+                resp.sendRedirect("index.jsp");//no pasa
+            }// redireccion a home
         } catch (SQLException throwables) {
             try {
                 ResultSet res = idpDB.getProfesorById("ID INGRESADO");
                 if (res.getString("contraseña_p") != "*CONTRASEÑA INGRESADA*") {
                     resp.sendRedirect("index.jsp");
-                }
+                }// redireccion a home
             }catch (SQLException throwables1) {
                 resp.sendRedirect("index.jsp");
             }
