b0d8776b797a16e52cb2a7267d4b3ea1286ffadf
santiagoHV <santiherreravelas@gmail.com>
2020-08-19 18:24:23 -0500

    intento de verificacion de contraseña

--- out/artifacts/Proyecto_final_PA_war_exploded/assets/functions-front.js
+++ out/artifacts/Proyecto_final_PA_war_exploded/assets/functions-front.js
@@ -5,3 +5,16 @@ function distribuirUsuario(papel) {
         document.getElementById("verificacionPreRegistro").value = "profesor";
     }
 }
+
+ function validarFormulario() {
+    var formulario = document.addForm;
+    if(formulario.inputContraseña.value!=formulario.inputContraseña2.value){
+        //CODIGO DE MENSAJE DE ERROR
+        console.log("CONTRASEÑAS DIFERENTES");
+        formulario.inputContraseña.value="";
+        formulario.inputContraseña1.value="";
+        return false;
+    }else{
+        return true;
+    }
+ }
--- web/assets/functions-front.js
+++ web/assets/functions-front.js
@@ -10,7 +10,11 @@ function distribuirUsuario(papel) {
     var formulario = document.addForm;
     if(formulario.inputContraseña.value!=formulario.inputContraseña2.value){
         //CODIGO DE MENSAJE DE ERROR
+        console.log("CONTRASEÑAS DIFERENTES");
         formulario.inputContraseña.value="";
         formulario.inputContraseña1.value="";
+        return false;
+    }else{
+        return true;
     }
  }
